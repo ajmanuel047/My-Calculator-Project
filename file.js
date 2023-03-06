8ed11848e51df1d0a15472fe40697c4d4b1a943a
Unuafe Ajiri <aj4short@rocketmail.com>
2023-03-06 09:27:04 +0100

    make the screen display more than a single number at a time

--- file.js
+++ file.js
@@ -13,10 +13,15 @@ const subtract = document.querySelector('.subtract');
 const add = document.querySelector('.addition');
 const multiply = document.querySelector('.multiply');
 const equal = document.querySelector('.equal');
-const plus_minus = document.querySelector('.plus_minus');
+const plusMinus = document.querySelector('.plus_minus');
 const clearButton = document.querySelector('.ac');
 const percentage = document.querySelector('.percentage');
-const decimal = document.querySelector('.decimal')
+const decimal = document.querySelector('.decimal');
+const displayScreen = document.querySelector('.display_screen');
+const buttonsContainer = document.querySelector('.buttons_container');
+
+displayScreen.textContent = 0
+
 
 let addition = (num1, num2) => {
     return num1 + num2;
@@ -40,43 +45,84 @@ let operate = (operator, num1, num2) => {
 operate(multiplication, 2, 4);
 
 zero.addEventListener('click', function(){
-
+    if(displayScreen.textContent == 0){
+        displayScreen.textContent = 0;
+    } else {
+            displayScreen.textContent += 0;
+    }; 
+    
 });
 
 one.addEventListener('click', function(){
-
+    if(displayScreen.textContent == 0){
+        displayScreen.textContent = 1;
+    } else {
+        displayScreen.textContent += 1;
+    }
 });
 
 two.addEventListener('click', function(){
-
+    if(displayScreen.textContent == 0){
+        displayScreen.textContent = 2;
+    } else {
+            displayScreen.textContent += 2;
+    }; 
 });
 
 three.addEventListener('click', function(){
-
+    if(displayScreen.textContent == 0){
+        displayScreen.textContent = 3;
+    } else {
+            displayScreen.textContent += 3;
+    }; 
 });
 
 four.addEventListener('click', function(){
-
+    if(displayScreen.textContent == 0){
+        displayScreen.textContent = 4;
+    } else {
+            displayScreen.textContent += 4;
+    }; 
 });
 
 five.addEventListener('click', function(){
-
+    if(displayScreen.textContent == 0){
+        displayScreen.textContent = 5;
+    } else {
+            displayScreen.textContent += 5;
+    }; 
 });
 
 six.addEventListener('click', function(){
-
+    if(displayScreen.textContent == 0){
+        displayScreen.textContent = 6;
+    } else {
+            displayScreen.textContent += 6;
+    }; 
 });
 
 seven.addEventListener('click', function(){
-
+    if(displayScreen.textContent == 0){
+        displayScreen.textContent = 7;
+    } else {
+            displayScreen.textContent += 7;
+    }; 
 });
 
 eight.addEventListener('click', function(){
-
+    if(displayScreen.textContent == 0){
+         displayScreen.textContent = 8;
+    } else {
+            displayScreen.textContent += 8;
+    }; 
 });
 
 nine.addEventListener('click', function(){
-
+    if(displayScreen.textContent == 0){
+        displayScreen.textContent = 9;
+    } else {
+            displayScreen.textContent += 9;
+    }; 
 });
 
 divide.addEventListener('click', function(){
@@ -100,14 +146,14 @@ equal.addEventListener('click', function(){
 });
 
 clearButton.addEventListener('click', function(){
-
+  
 });
 
 percentage.addEventListener('click', function(){
 
 })
 
-plus_minus.addEventListener('click', function(){
+plusMinus.addEventListener('click', function(){
 
 });
 
