a29906d2a717eb34712d6f3ee77d100b88f9a676
Unuafe Ajiri <aj4short@rocketmail.com>
2023-03-29 14:38:55 +0100

    make your time to always be current

--- file.js
+++ file.js
@@ -19,13 +19,32 @@ const percentage = document.querySelector('.percentage');
 const decimal = document.querySelector('.decimal');
 const displayScreen = document.querySelector('.display_screen');
 const buttonsContainer = document.querySelector('.buttons_container');
+const time = document.querySelector('.time')
 
-displayScreen.textContent = 0
+// let myButton = document.getElementById("myButton");
+// let myVariable;
 
+// myButton.addEventListener("click", function() {
+//   myVariable = 1;
+//   console.log(myVariable);
+// });
 
-let addition = (num1, num2) => {
-    return num1 + num2;
-};
+ let currDate = new Date();
+ let hoursMin = currDate.getHours() + ':' + currDate.getMinutes();
+ time.textContent = hoursMin
+
+displayScreen.textContent = 0;
+let displayScreenValues = displayScreen;
+let num1 = 0;
+let num2 = 0;
+// console.log(num1)
+// // console.log(num2)
+// console.log(num1 - num2)
+let addition = () => {
+    return '+' ;
+}; 
+//  addition(num1, num2)
+
 
 let subtraction = (num2, num1) => {
     return num2 - num1;
@@ -40,17 +59,23 @@ let division = (num1, num2) => {
 };
 
 let operate = (operator, num1, num2) => {
-    console.log(operator(num1, num2));
+    console.log(num1)
+  
+    if(operator() == '+'){
+        console.log('+')
+        console.log(typeof Number(num1))
+       return Number(num1) +Number(num2)
+    }
+        
 };
-operate(multiplication, 2, 4);
+
 
 zero.addEventListener('click', function(){
     if(displayScreen.textContent == 0){
         displayScreen.textContent = 0;
     } else {
             displayScreen.textContent += 0;
-    }; 
-    
+    };     
 });
 
 one.addEventListener('click', function(){
@@ -59,6 +84,15 @@ one.addEventListener('click', function(){
     } else {
         displayScreen.textContent += 1;
     }
+    
+    if(num1 == 0){
+        num1 = '1'
+    } else {
+        num1 += '1'
+    }
+   
+     console.log(num1)
+     
 });
 
 two.addEventListener('click', function(){
@@ -67,7 +101,17 @@ two.addEventListener('click', function(){
     } else {
             displayScreen.textContent += 2;
     }; 
+    if(num2 == 0){
+        num2 = '2'
+    } else {
+        num2 += '2'
+    }
+    
+    
+    console.log(num2)
+    
 });
+
 
 three.addEventListener('click', function(){
     if(displayScreen.textContent == 0){
@@ -134,7 +178,7 @@ multiply.addEventListener('click', function(){
 });
 
 add.addEventListener('click', function(){
-document.body.style.backgroundColor = 'green'
+    console.log(addition())
 });
 
 subtract.addEventListener('click', function(){
@@ -142,11 +186,18 @@ subtract.addEventListener('click', function(){
 });
 
 equal.addEventListener('click', function(){
-
+       displayScreen.textContent = (operate(addition, num1, num2))
+    // displayScreen.textContent = (operate(addition, 3, 4))
+    // displayScreen.textContent = (operate(multiplication, 3, 4))
+    
 });
 
 clearButton.addEventListener('click', function(){
-  
+    displayScreen.textContent = 0;
+  num1 = 0;
+  num2 = 0;
+  console.log(num1)
+  console.clear()
 });
 
 percentage.addEventListener('click', function(){
